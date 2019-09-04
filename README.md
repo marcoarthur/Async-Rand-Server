7a5cb4890a2c106d623ed0afd611583a0afbc9e7
Marco Arthur <arthurpbs@gmail.com>
2019-09-04 15:30:58 -0300

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Async::Rand::Server - Blah blah blah
+
+# SYNOPSIS
+
+    use Async::Rand::Server;
+
+# DESCRIPTION
+
+Async::Rand::Server is
+
+# AUTHOR
+
+Marco Arthur <arthurpbs@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2019- Marco Arthur
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Async/Rand/Server.pm
@@ -0,0 +1,39 @@
+package Async::Rand::Server;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Async::Rand::Server - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Async::Rand::Server;
+
+=head1 DESCRIPTION
+
+Async::Rand::Server is
+
+=head1 AUTHOR
+
+Marco Arthur E<lt>arthurpbs@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2019- Marco Arthur
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
